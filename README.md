908eda0a713db2633add8c75ddc289b024f57845
Freewind <nowindlee@gmail.com>
2020-11-23 21:05:18 +0800

    typescript-pretty-format-demo

--- README.md
+++ README.md
@@ -1,18 +1,9 @@
-TypeScript Hello World Demo
+TypeScript Pretty Format Demo
 ===========================
+
+'prettyFormat'可以处理比较复杂的数据
 
 ```
 npm install
 npm run demo
 ```
-
-Note:
-
-`esModuleInterop` is recommended to set to `true`,
-since we can have consistent importing syntax with babel,
-always use:
-
-```
-import some from 'some'
-```
-
--- hello.ts
+++ hello.ts
@@ -1,1 +1,17 @@
-console.log('Hello');
+import prettyFormat from 'pretty-format';
+
+const data: any = {
+  aaa: 123,
+  bbb: undefined,
+  ccc: null,
+  hello: () => '',
+}
+
+console.log("### prettyFormat", prettyFormat(data))
+console.log("### JSON.stringify", JSON.stringify(data));
+
+
+console.log("---------- nested --------------")
+data.nest = data;
+console.log("### prettyFormat", prettyFormat(data))
+console.log("### JSON.stringify", JSON.stringify(data));
